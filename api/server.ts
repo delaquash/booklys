cfcd13147b585b3528d1d992a1f237217aaf9c03
delaquash <olaide1191@gmail.com>
2023-02-09 14:45:33 +0100

    feat: add route

--- api/server.ts
+++ api/server.ts
@@ -1,11 +1,24 @@
-import express, { Express, Request, Response } from "express";
+import express, {  Request, Response } from "express";
 import connectDB from "./config/db";
 import config from "config";
+import authRoute from "./routes/auth";
+import userRoute from "./routes/user";
+import hotelRoute from "./routes/hotels";
+import roomRoute from "./routes/rooms";
 import logger  from "./logger";
-
 
 const PORT = config.get<number>("PORT")
 const app = express();
+
+app.get("/", (req:Request, res:Response)=> {
+    res.send("Welcome")
+})
+
+// middleware
+app.use("/auth", authRoute);
+app.use("/user", userRoute);
+app.use("/room", roomRoute);
+app.use("/hotel", hotelRoute);
 
 app.listen(PORT, async ()=> {
     logger.info(`Server running in mode on ${PORT}`);
