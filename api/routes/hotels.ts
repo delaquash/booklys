b7f1f894a0018da4ce30b5619c84055c78dd49e5
delaquash <olaide1191@gmail.com>
2023-02-09 16:07:29 +0100

    feat: add hotel post route

--- api/routes/hotels.ts
+++ api/routes/hotels.ts
@@ -1,8 +1,15 @@
 import express, {  Request, Response } from "express";
-const router = express.Router()
+import Hotel from "../models/Hotel";
+const router = express.Router();
 
-router.get("/", (req:Request, res:Response)=> {
-    res.send("Hello, this is the auth endpoint!")
+router.post("/", async (req:Request, res:Response)=> {
+    const newHotel = new Hotel(req.body)
+    try {
+        const savedHotel =await newHotel.save()
+        res.status(201).json(savedHotel)
+    } catch (error) {
+        res.status(500).json(error)
+    }
 })
 
 export default router;
