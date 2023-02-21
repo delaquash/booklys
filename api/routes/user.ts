892d7e0d688dd8e6667f5604bb1642cad92f07db
delaquash <olaide1191@gmail.com>
2023-02-21 08:15:02 +0100

    fixed: all user route

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,6 +1,14 @@
-import express, {  Request, Response } from "express";
+import express from "express";
+import { deleteUser, findAllUsers, findUser, updateUser } from "../controllers/User";
 const router = express.Router()
 
-// router.get("/",)
+// privately update a user
+router.get("/:id", updateUser);
+// privately delete user
+router.delete("/:id", deleteUser);
+// publicly find all user
+router.get("/", findAllUsers);
+// privately find user
+router.get("/:id", findUser)
 
 export default router;
