cb40773bec45f42181b6b826b8b08f705b7b1c33
delaquash <olaide1191@gmail.com>
2023-02-21 16:34:33 +0100

    fixed: verifyToken

--- api/routes/user.ts
+++ api/routes/user.ts
@@ -1,9 +1,20 @@
 import express from "express";
+import { Response, Request, NextFunction } from "express";
 import { deleteUser, findAllUsers, findUser, updateUser } from "../controllers/User";
+import { verifyToken, verifyUser } from "../utils/verifyToken";
 const router = express.Router()
 
+router.get("/checkAuth", verifyToken, (req: Request, res:Response, next:NextFunction)=> {
+    res.send("You are a verified user.")
+})
+
+router.get("/checkUser", verifyUser,(req: Request, res: Response) => {
+    res.send("You are verified token")
+})
+
+
 // privately update a user
-router.post("/:id", updateUser);
+router.put("/:id", updateUser);
 // privately delete user
 router.delete("/:id", deleteUser);
 // publicly find all user
--- api/utils/verifyToken.ts
+++ api/utils/verifyToken.ts
@@ -2,17 +2,47 @@ import jwt from "jsonwebtoken";
 import ErrorException from "./error";
 import { Response, Request, NextFunction } from "express";
 
-export const verifyToken = (req: Request, res: Response, next:NextFunction) => {
-    /* Getting the token from the cookie. */
-    const token = req.cookies.access_token;
-    /* Checking if the token is not present, then it will throw an error. */
-    if(!token) {
-        return next(new ErrorException(401, "You are not authorised to perform this function."))
-    }
+export const verifyToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  /* Getting the token from the cookie. */
+  const token = req.cookies.access_token;
+  /* Checking if the token is not present, then it will throw an error. */
+  if (!token) {
+    return next(
+      new ErrorException(
+        401,
+        "You are not authorised to perform this function."
+      )
+    );
+  }
 
-    jwt.verify(token, process.env.JWT_SECRET ?? "", (err: any, user: any) => {
-        if(err) return next(new ErrorException(403, "Token not valid"));
-        req.user = user;
-        next()
-    })
-}
+  jwt.verify(token, process.env.JWT_SECRET ?? "", (err: any, user: any) => {
+    if (err) return next(new ErrorException(403, "Token not valid"));
+    req.user = user;
+    next();
+  });
+};
+
+export const verifyUser = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  verifyToken(req, res, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      if (err)
+        return next(
+          new ErrorException(
+            403,
+            "You are not authorised to perform this action."
+          )
+        );
+    }
+  });
+};
