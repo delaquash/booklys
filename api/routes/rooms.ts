a1a7bc954b3048bee68da855d6b1a92b44e70d4f
delaquash <olaide1191@gmail.com>
2023-02-23 19:46:30 +0100

    feat: add room route

--- api/routes/rooms.ts
+++ api/routes/rooms.ts
@@ -1,8 +1,17 @@
-import express, {  Request, Response } from "express";
+import express from "express";
 const router = express.Router()
+import { isAdmin } from "../utils/verifyToken";
+import { createRoom, deleteRoom, findAllRoom, findRoom, updateRoom } from "../controllers/Room";
 
-router.get("/", (req:Request, res:Response)=> {
-    res.send("Hello, this is the auth endpoint!")
-})
+// creating new hotel
+router.post("/", isAdmin, createRoom);
+// deleting a Room
+router.delete("/:id", isAdmin, deleteRoom);
+// updating a Room
+router.put("/:id", isAdmin, updateRoom);
+// get all Rooms
+router.get("/", findAllRoom);
+// get single hostel
+router.get("/:id", findRoom);
 
 export default router;
