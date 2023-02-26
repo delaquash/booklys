817c2088358fa59f7f6ffcd2fafc31d5f38a0613
delaquash <olaide1191@gmail.com>
2023-02-26 21:42:51 +0100

    feat: add home,navbar and header

---
+++ client/src/App.tsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home";
+import List from "./Pages/List";
+import Hotel from "./Pages/Hotel";
+
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/hotel" element={<List />} />
+      <Route path="/hotel/:id" element={<Hotel />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ client/src/Components/Header.tsx
@@ -0,0 +1,41 @@
+import {
+  faBed,
+  faCab,
+  faPlane,
+  faTaxi,
+} from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import "../Styles/Header.css";
+
+const Header = () => {
+  return (
+    <div className="header">
+      <div className="headerContainer">
+        <div className="headerList">
+          <div className="headerListItem active">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Stays</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faPlane} />
+            <span>Flights</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faCab} />
+            <span>Car Rentals</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faBed} />
+            <span>Attractions</span>
+          </div>
+          <div className="headerListItem">
+            <FontAwesomeIcon icon={faTaxi} />
+            <span>Airpot taxis</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
