4a33b18192c0132aac97bb20c6b48a45080fcfaa
delaquash <olaide1191@gmail.com>
2023-03-08 21:52:51 +0100

    fixed: searchItem component

--- client/src/Components/SearchItem.tsx
+++ client/src/Components/SearchItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import "../Styles/SearchItem.css";
 
 const SearchItem = () => {
   return (
@@ -6,7 +6,7 @@ const SearchItem = () => {
       <img
         src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
         alt=""
-        className="featuredImg"
+        className="sImg"
       />
       <div className="iDesc">
         <h1 className="siTitle">Tower Street Apartment</h1>
@@ -20,11 +20,19 @@ const SearchItem = () => {
         </span>
         <span className="siCancelOp">Free Cancellation</span>
         <span className="isCanOpSubtitle">
-          You can cancel later, so lock up this great offer today
+          You can cancel later, so take this great offer today
         </span>
       </div>
       <div className="siDetails">
-        
+        <div className="siRatings">
+          <span>Excellent</span>
+          <button>8.9</button>
+        </div>
+        <div className="siDetails">
+          <span className="siPrice">$120</span>
+          <span className="siTaxiOp">Include taxes and fee</span>
+          <button className="siCheckButton">Check Availabilty</button>
+        </div>
       </div>
     </div>
   );
