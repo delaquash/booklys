977cbef153c0fb2c3bd87374ba0afd4ee8345f1b
delaquash <olaide1191@gmail.com>
2023-03-09 08:37:43 +0100

    fixed: searchItem component

--- client/src/Components/SearchItem.tsx
+++ client/src/Components/SearchItem.tsx
@@ -3,35 +3,44 @@ import "../Styles/SearchItem.css";
 const SearchItem = () => {
   return (
     <div className="searchItem">
-      <img
-        src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
-        alt=""
-        className="sImg"
-      />
-      <div className="iDesc">
-        <h1 className="siTitle">Tower Street Apartment</h1>
-        <span className="siDistance">500m from center</span>
+      <img  src="https://cf.bstatic.com/xdata/images/city/max500/957801.webp?k=a969e39bcd40cdcc21786ba92826063e3cb09bf307bcfeac2aa392b838e9b7a5&o="
+        alt=""className="siImg" />
+      <div className="siDesc">
+        <h1 className="siTitle">
+          {/* {item.name} */}
+          </h1>
+        <span className="siDistance">
+          {/* {item.distance} */}
+          m from center</span>
         <span className="siTaxiOp">Free airport taxi</span>
         <span className="siSubtitle">
-          Studio Apartment with Air Conditioning
+          Studio Apartment with Air conditioning
         </span>
         <span className="siFeatures">
-          Entire Studio-1 bathroom-21m Full bed
-        </span>
-        <span className="siCancelOp">Free Cancellation</span>
-        <span className="isCanOpSubtitle">
-          You can cancel later, so take this great offer today
+          {/* {item.desc} */}Description
+          </span>
+        <span className="siCancelOp">Free cancellation </span>
+        <span className="siCancelOpSubtitle">
+          You can cancel later, so lock in this great price today!
         </span>
       </div>
       <div className="siDetails">
-        <div className="siRatings">
+        {/* {item.rating &&  */}
+        <div className="siRating">
           <span>Excellent</span>
-          <button>8.9</button>
+          <button>
+            {/* {item.rating} */}
+            </button>
         </div>
-        <div className="siDetails">
-          <span className="siPrice">$120</span>
-          <span className="siTaxiOp">Include taxes and fee</span>
-          <button className="siCheckButton">Check Availabilty</button>
+        {/* } */}
+        <div className="siDetailTexts">
+          <span className="siPrice">$
+          {/* {item.cheapestPrice} */}
+          </span>
+          <span className="siTaxOp">Includes taxes and fees</span>
+          {/* <Link to={`/hotels/${item._id}`}> */}
+          <button className="siCheckButton">See availability</button>
+          {/* </Link> */}
         </div>
       </div>
     </div>
