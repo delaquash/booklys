9e1bd023fed9b8ce97e2dca1205793df3b7b627a
delaquash <olaide1191@gmail.com>
2023-03-15 15:33:29 +0100

    feat: add usefetch component

---
+++ client/src/Hooks/useFetch.tsx
@@ -0,0 +1,36 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useFetch = (url: string) => {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [error, setError] = useState<boolean>(false);
+  const [data, setData] = useState<string[]>([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      // setLoading(true)
+      try {
+        const res =await axios.get(url);
+        setData(res.data);
+      } catch (err) {
+        setError(err)
+      }
+      setLoading(false);
+    };
+    fetchData();
+  }, [url]);
+
+  const reFetch = async () => {
+    setLoading(true);
+    try {
+      const res = axios.get(url);
+      setData(res.data);
+    } catch (err) {
+      setError(err);
+    }
+    setLoading(false);
+  };
+  return { data, loading, error, reFetch}
+};
+
+export default useFetch;
