d6536eaae8396808ec8986d32375e7b0af1f30ee
delaquash <olaideemmanuel0@gmail.com>
2024-02-02 14:56:47 +0000

    feat: add footer

--- client/src/layout/Layout.tsx
+++ client/src/layout/Layout.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../components/Header';
+import Hero from '../components/Hero';
 
 const Layout = () => {
   return (
     <div className='flex flex-col min-h-screen'>
         <Header />
+        <Hero />
     </div>
   )
 }
