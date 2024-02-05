74f9231c698f76a2000aac58adc58682823fa0db
delaquash <olaideemmanuel0@gmail.com>
2024-02-05 14:23:30 +0000

    feat: add login route

--- api/controllers/Auth.ts
+++ api/controllers/Auth.ts
@@ -17,7 +17,33 @@ const login = async (req: Request, res: Response) => {
     if (!errors.isEmpty()) {
         return res.status(400).json({ errors: errors.array() });
     }
-    const { email, password } = req.body
+    const { email, password } = req.body;
+    try {
+        const user = await User.findOne({ email })
+        if(!user){
+            return res.status(401).json({ msg: "Invalid credentials"})
+        }
 
+        const isMatch = bcrypt.compare(password, user.password)
+        if(!isMatch){
+            return res.status(400).json({msg:"Invalid credentials"});
+        }
+
+        const token = jwt.sign(
+            { userId: user.id },
+            process.env.JWT_SECRET_KEY as string,
+            {
+              expiresIn: "60d",
+            }
+          );
+          res.cookie("auth_token", token, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            maxAge: 86400000,
+          });
+          res.status(200).json({ userId: user._id });
+    } catch (error) {
+        
+    }
 
 }
