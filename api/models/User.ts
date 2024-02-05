e337f9b58f29278b38d359731771bd9344a20913
delaquash <olaideemmanuel0@gmail.com>
2024-02-05 12:05:50 +0000

    feat: add user and hotel model for db

--- api/models/User.ts
+++ api/models/User.ts
@@ -4,51 +4,57 @@ interface MongoResult {
   _doc: any;
 }
 interface IUser extends MongoResult {
-  username: string;
+  // username: string;
   email: string;
-  country: string;
-  img: string;
-  city: string;
-  phone: string;
+  firstName: string;
+  lastName: string;
+  // country: string;
+  // img: string;
+  // city: string;
+  // phone: string;
   password: string;
-  isAdmin: boolean;
+  // isAdmin: boolean;
 }
 
 const UserSchema = new Schema<IUser>(
   {
-    username: {
-      type: String,
-      required: true,
-      unique: true,
-    },
+    // username: {
+    //   type: String,
+    //   required: true,
+    //   unique: true,
+    // },
     email: {
       type: String,
       required: true,
       unique: true,
     },
-    country: {
+    firstName: {
       type: String,
       required: true,
     },
-    img: {
-      type: String,
-    },
-    city: {
-      type: String,
-      required: true,
-    },
-    phone: {
-      type: String,
-      required: true,
-    },
+    // country: {
+    //   type: String,
+    //   required: true,
+    // },
+    // img: {
+    //   type: String,
+    // },
+    // city: {
+    //   type: String,
+    //   required: true,
+    // },
+    // phone: {
+    //   type: String,
+    //   required: true,
+    // },
     password: {
       type: String,
       required: true,
     },
-    isAdmin: {
-      type: Boolean,
-      default: false,
-    },
+    // isAdmin: {
+    //   type: Boolean,
+    //   default: false,
+    // },
   },
   { timestamps: true }
 );
