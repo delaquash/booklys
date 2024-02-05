02cf0b8eba5d8f8dd37dd4c5f0fc7e11c86d3bf3
delaquash <olaideemmanuel0@gmail.com>
2024-02-05 11:03:30 +0000

    fix: props-layout-component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,14 +5,27 @@ import Layout from './layout/Layout'
 const App = () => {
   return (
     // <Router>
-      <Routes>
-        <Route path="/" element={<Layout></Layout>}/>
-        <Route path='/search' element="/" />
-        <Route path='*' element={<Navigate to="/" />}/>
-      </Routes>
+    <Routes>
+      <Route
+        path="/"
+        element={
+          <Layout>
+            <p>Home Page</p>
+          </Layout>
+        }
+      />
+      <Route
+        path="/search"
+        element={
+          <Layout>
+            <p>Search Page</p>
+          </Layout>
+        }
+      />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
     // </Router>
-    
-  )
+  );
 }
 
 export default App
--- client/src/components/Footer.tsx
+++ client/src/components/Footer.tsx
@@ -1,3 +1,5 @@
+
+
 const Footer = () => {
     return (
       <div className="bg-blue-800 py-10">
