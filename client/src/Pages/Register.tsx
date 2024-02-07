1e9c72a837444f1ee3c1154b9ba1f0d7aee44bd7
delaquash <olaideemmanuel0@gmail.com>
2024-02-07 13:56:13 +0000

    feat: add register button

--- client/src/Pages/Register.tsx
+++ client/src/Pages/Register.tsx
@@ -50,6 +50,35 @@ const Register = () => {
           </span>
         {/* )} */}
       </label>
+      <label className="text-gray-700 text-sm font-bold flex-1">
+        Confirm Password
+        <input
+          type="password"
+          className="border rounded w-full py-1 px-2 font-normal"
+          {...register("confirmPassword", {
+            validate: (val) => {
+              if (!val) {
+                return "This field is required";
+              } else if (watch("password") !== val) {
+                return "Your passwords do no match";
+              }
+            },
+          })}
+        ></input>
+        {/* {errors.confirmPassword && ( */}
+          <span className="text-red-500">
+            {/* {errors.confirmPassword.message} */}
+            </span>
+        {/* )} */}
+      </label>
+      <span>
+        <button
+          type="submit"
+          className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
+        >
+          Create Account
+        </button>
+        </span>
         </div>
     </form>
   )
