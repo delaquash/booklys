b371b00acb65fe22a17acc36efe2f5dd42f66f60
delaquash <olaide1191@gmail.com>
2024-02-10 13:06:15 +0000

    feat: add token for validation

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { login } from "../controllers/Auth";
+import { login, validateToken } from "../controllers/Auth";
 
 const router = express.Router();
 
 // router.post("/register", register);
 router.post("/login", login);
+router.get("/validate-token", validateToken)
 
 export default router;
