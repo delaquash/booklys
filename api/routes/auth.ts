60731cd0cc72253dbaef1bc739cdfacc5e691a8f
delaquash <olaide1191@gmail.com>
2024-02-15 19:14:32 +0000

    fix: login

--- api/routes/auth.ts
+++ api/routes/auth.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { login, validateToken } from "../controllers/Auth";
+import { login,  validateToken } from "../controllers/Auth";
 import verifyToken from "../middleware/auth";
 
 const router = express.Router();
 
 router.post("/login", login);
-router.get("/validate-token", verifyToken, validateToken)
+router.get("/validate-token", verifyToken, validateToken);
+
 
 export default router;
