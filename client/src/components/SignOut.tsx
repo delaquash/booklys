93812d2afc1bcb43f7ac7d27a395afe543955c40
delaquash <olaide1191@gmail.com>
2024-02-16 16:28:10 +0000

    feat: add sign out functionality

--- client/src/components/SignOut.tsx
+++ client/src/components/SignOut.tsx
@@ -1,7 +1,23 @@
+import { useActionData } from "react-router-dom";
+import { useAppContext } from "../context/AppContext"
+import * as apiClient from "../Hooks/api-client";
+import { useMutation } from "react-query";
 
 const SignOut = () => {
+    const {showToast} = useAppContext();
+    const mutation = useMutation(apiClient.signout, {
+        onSuccess:()=>{},
+        onError:()=>{}
+    })
+
+    const handleClick =()=> {
+        mutation.mutate()
+    }
+
   return (
-    <button className="text-blue-600 px-3 font-bold bg-white hover:bg-gray-100">
+    <button
+
+        className="text-blue-600 px-3 font-bold bg-white hover:bg-gray-100">
         Sign Out
     </button>
   )
