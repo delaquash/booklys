ed94d53698808c92af21cca54f91f457c7c175d7
delaquash <olaide1191@gmail.com>
2024-03-07 02:36:34 +0000

    feat: add cloudinary endpoint

--- api/src/controllers/Hotel.ts
+++ api/src/controllers/Hotel.ts
@@ -1,14 +1,32 @@
 import { NextFunction, Request, Response } from "express";
-import multer from "multer";
-// import Hotel from "../models/Hotel";
+import cloudinary from "cloudinary";
+import UploadImage from "../utils/CloudinaryStorage";
+import { HotelType } from "../types/data";
+import Hotel from "../models/Hotel";
 
 
-
-// export const createHotel = async (
-//   req: Request,
-//   res: Response,
-//   next: NextFunction
-// ) => {
+export const createHotel = UploadImage.array("imageFiles", 6), async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const imageFiles = req.files as Express.Multer.File[];
+        const newHotel: HotelType = req.body;
+        // upload images to cloudinary
+        const uploadPromises = imageFiles.map(async(image)=> {
+            const b64 = Buffer.from(image.buffer).toString("base64");
+            let dataURI = "data:" + image.mimetype + ";base64," + b64;
+            const res = await cloudinary.v2.uploader.upload(dataURI);
+            return res.url
+        });
+        //  add the url of each uploaded file to hotel object
+        const imageUrls = await Promise.all(uploadPromises);
+        newHotel.imageUrls = imageUrls;
+        newHotel.lastUpdated = new Date();
+        newHotel.userId = req.userId;
+     
+        
+    } catch (error) {
+        console.log("Error creating hotel: ", error)
+        res.status(500).json({ message: "Server Error"})
+    }
 //   const newHotel = new Hotel(req.body);
 //   try {
 //     const savedHotel = await newHotel.save();
@@ -16,7 +34,7 @@ import multer from "multer";
 //   } catch (err) {
 //     next(err);
 //   }
-// };
+};
 
 // export const updateHotel = async (
 //   req: Request,
--- api/src/utils/CloudinaryStorage.ts
+++ api/src/utils/CloudinaryStorage.ts
@@ -1,9 +1,11 @@
 import multer from "multer";
 
 const storage = multer.memoryStorage()
-export const uploadImage = multer({
+ const UploadImage = multer({
     storage:storage,  //multers disk storage destination
     limits: {
         fileSize: 5 * 1024 *1024 //maximum of 5MB file 
     }
 })
+
+export default UploadImage;
