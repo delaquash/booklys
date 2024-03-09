3d0622499558f5c4a2adbb8abe75b75e0e7ded44
delaquash <olaide1191@gmail.com>
2024-03-09 15:11:27 +0000

    feat: add manage-hotel-form page

---
+++ client/src/forms/ManageHotelForms/ManageHotelsForms.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { useForm } from 'react-hook-form';
+
+
+
+const ManageHotelsForms = () => {
+  return (
+    <div>ManageHotelsForms</div>
+  )
+}
+
+export default ManageHotelsForms
