a9ccb2ae57bacbc6c64989762a98f96c4ee22f59
delaquash <olaide1191@gmail.com>
2024-03-14 23:17:08 +0000

    feat: add functionality to facilities section

--- client/src/forms/ManageHotelForms/FacilitiesSection.tsx
+++ client/src/forms/ManageHotelForms/FacilitiesSection.tsx
@@ -14,12 +14,23 @@ const FacilitiesSection = () => {
                     <input 
                         type='checkbox'
                         value={facility}
-                        {...register("facilities")}
+                        {...register("facilities", {
+                            validate: (facilities)=> {
+                                if(facilities && facilities.length > 0){
+                                    return true;
+                                } else {
+                                    return "At least one facility is required"
+                                }
+                            }
+                        })}
                     />
                     {facility}
                 </label>
             ))}
         </div>
+        {errors.facilities && (
+            <span className="text-red-500 text-sm font-bold">{errors.facilities.message}</span>
+        )}
     </div>
   )
 }
