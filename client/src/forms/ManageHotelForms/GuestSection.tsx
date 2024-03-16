3ed721b35eac9081645b0f5ea6deab9be6942440
delaquash <olaide1191@gmail.com>
2024-03-16 23:50:13 +0000

    fix: initial commit

---
+++ client/src/forms/ManageHotelForms/GuestSection.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { useFormContext } from 'react-hook-form';
+import { HotelFormDataProps } from './ManageHotelsForms';
+
+const GuestSection = () => {
+    const {register,watch, formState: {errors}} = useFormContext<HotelFormDataProps>();
+  return (
+    <div>
+        <h2 className="mb-3 font-bold text-2xl">Guest</h2>
+        <div>
+            
+        </div>
+    </div>
+  )
+}
+
+export default GuestSection;
