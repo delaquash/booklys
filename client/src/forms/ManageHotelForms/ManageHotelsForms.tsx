b7b3986c492a323ed58ded349b2a1ddb8bd6fdde
delaquash <olaide1191@gmail.com>
2024-03-20 09:59:11 +0000

    feat: add form functionality to manageHotelForms page

--- client/src/forms/ManageHotelForms/ManageHotelsForms.tsx
+++ client/src/forms/ManageHotelForms/ManageHotelsForms.tsx
@@ -22,9 +22,30 @@ export type HotelFormDataProps = {
 
 const ManageHotelsForms = () => {
     const formMethods = useForm<HotelFormDataProps>()
-  return (
+    const { handleSubmit } = formMethods
+    const onSubmit = handleSubmit ((formDataJson:HotelFormDataProps)=>{
+      const formData = new FormData();
+      formData.append("name", formDataJson.name)
+      formData.append("city", formDataJson.city)
+      formData.append("country", formDataJson.country)
+      formData.append("description", formDataJson.description)
+      formData.append("type", formDataJson.type)
+      formData.append("pricePerNight", formDataJson.pricePerNight.toString())
+      formData.append("starRating", formDataJson.starRating.toString())
+      formData.append("adultCount", formDataJson.adultCount.toString())
+      formData.append("childCount", formDataJson.childCount.toString())
+
+      formDataJson.facilities.forEach((facility, index)=>{
+        formData.append(`facilities[${index}]`, facility);
+      });
+
+      Array.from(formDataJson.imageFiles).forEach((imageFile)=> {
+        formData.append(`imageFiles`, imageFile);
+      });
+    });
+      return (
     <FormProvider {...formMethods}>
-        <form className='flex flex-col gap-10'>
+        <form className='flex flex-col gap-10' onSubmit={onSubmit}>
             <Detailssection />
             <TypeSection/>
             <FacilitiesSection />
