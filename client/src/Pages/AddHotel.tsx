0abc1e1a905cba62355e431576dbdfd40731f849
delaquash <olaide1191@gmail.com>
2024-03-20 10:13:49 +0000

    feat: add form details

--- client/src/Pages/AddHotel.tsx
+++ client/src/Pages/AddHotel.tsx
@@ -1,10 +1,27 @@
 import React from 'react'
 import ManageHotelsForms from '../forms/ManageHotelForms/ManageHotelsForms'
+import { useMutation } from 'react-query'
+import { useAppContext } from '../context/AppContext'
+import * as apiClient from "../Hooks/api-client"
 
 
 function AddHotel() {
+  const { showToast } = useAppContext()
+  const {isLoading, mutate} = useMutation(apiClient.addMyHotel, {
+    onSuccess: () => {
+      showToast({message: "Hotel Saved", type: "SUCCESS"})
+    },
+    onError: () => {
+      showToast({ message: "Error Saving Hotel", type: "ERROR"})
+    }
+  })
+  const handleSave =(hotelFormData: FormData) => {
+    mutate(hotelFormData)
+  }
   return (
-    <ManageHotelsForms/>
+    <ManageHotelsForms
+        onSave={handleSave}
+    />
   )
 }
 
