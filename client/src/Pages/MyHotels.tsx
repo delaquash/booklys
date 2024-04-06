eca9651baec962314b50df46273bdb7d92a8c076
delaquash <olaide1191@gmail.com>
2024-04-06 14:01:24 +0000

    feat: add myhotels page

---
+++ client/src/Pages/MyHotels.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+
+const MyHotels = () => {
+  return (
+    <div className='space-y-5'>
+        <span>
+            <h1 className="text-3xl font-bold">My Hotels</h1>
+            <Link to="/add-hotel" className='flex bg-blue-600 text-white text-xl font-bold p-2 hover:bg-blue-500'>
+                Add Hotel
+            </Link>
+        </span>
+    </div>
+  )
+}
+
+export default MyHotels;
