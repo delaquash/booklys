4dbb618004ead4ecac5dd62a0891be8233a83d4f
delaquash <olaide1191@gmail.com>
2024-04-09 17:12:07 +0000

    feat: add params to edit hotel page

--- client/src/Pages/EditHotel.tsx
+++ client/src/Pages/EditHotel.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
+import { useQuery } from 'react-query';
+import { useParams } from 'react-router-dom';
+import * as apiClient from "../Hooks/api-client";
 
 const EditHotel = () => {
+    const { hotelId } = useParams();
+    const { data: hotel } = useQuery("editHotels", () => apiClient.editHotels(hotelId || ""),
+    {
+        enabled: !!hotelId,
+    })
   return (
     <div>EditHotel</div>
   )
