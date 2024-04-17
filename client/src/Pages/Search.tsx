d26c844f383d6ad29bc50d80a952e3c01ac56705
delaquash <olaide1191@gmail.com>
2024-04-17 14:50:29 +0000

    feat: add styling for search page

--- client/src/Pages/Search.tsx
+++ client/src/Pages/Search.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-
+import { useSearchContext } from '../context/SearchContext';
 
 const Search = () => {
+    const search = useSearchContext();
+    console.log(search)
   return (
     <div>Search</div>
   )
