97c57868dff8bf0e2dfceea9c172eeda402c0bc7
delaquash <olaide1191@gmail.com>
2024-04-17 12:51:16 +0000

    feat: add datapicker to checkout

--- client/src/components/Searchbar.tsx
+++ client/src/components/Searchbar.tsx
@@ -1,6 +1,8 @@
 import React, { FormEvent, useState } from 'react';
 import { useSearchContext } from '../context/SearchContext';
 import { MdTravelExplore } from "react-icons/md";
+import  DatePicker  from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css"
 
 const Searchbar = () => { 
     const search = useSearchContext();
@@ -21,7 +23,9 @@ const Searchbar = () => {
             childCount
         );
     };
-
+const minDate= new Date();
+const maxDate = new Date();
+maxDate.setFullYear(maxDate.getFullYear() + 1)
   return (
     <form onSubmit={handleSubmit}
         className='-mt-8 p-3 bg-orange-400 rounded shadow-md grid grid-cols-2 
@@ -50,16 +54,29 @@ const Searchbar = () => {
                 />
             </label>
             <label className="flex items-center">
-                Adults: 
+                Children: 
                 <input 
                     className='w-full p-1 focus:outline-none font-bold'
                     type='number'
                     min={1}
                     max={20}
-                    value={adultCount}
-                    onChange={(e)=>setAdultCount(parseInt(e.target.value))}
+                    value={childCount}
+                    onChange={(e)=>setChildCount(parseInt(e.target.value))}
                 />
             </label>
+        </div>
+        <div>
+            <DatePicker 
+                selected={checkIn}
+                onChange={(date)=>setCheckIn( date as Date)}
+                selectsStart
+                startDate={checkIn}
+                endDate={checkOut}
+                minDate={minDate}
+                maxDate={maxDate}
+                placeholderText='Check-in Date'
+                className='min-w-full bg-white p-2 focus:outline-none'
+            />
         </div>
     </form>
   )
