1b25253ab27afeac5d531a4a9dcd0ca46b9c313f
delaquash <olaide1191@gmail.com>
2024-04-18 13:40:49 +0000

    "Add link to hotel detail page and update SearchResultCard layout"
    This diff includes the following changes:
    
    * Importing `Link` from `react-router-dom`.
    * Wrapping the search result card content in a

--- client/src/components/SearchResultCard.tsx
+++ client/src/components/SearchResultCard.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { HotelType } from '../../types/dataTypes';
-import { AiFillStar } from "react-icons/ai"
+import { AiFillStar } from "react-icons/ai";
+import { Link } from 'react-router-dom';
+
 
 type SearchResultProps = {
     hotel: HotelType
@@ -8,53 +10,60 @@ type SearchResultProps = {
 
 const SearchResultCard = ({hotel}:SearchResultProps) => {
   return (
-    <div className="grid grid-cols-1 xl:grid-cols-[2fr_3fr] border border-slate-300 rounded-lg">
-        <div className="w-full h-[300px]">
-          <img 
-              className='object-cover object-center w-full h-full'
-              src={hotel.imageUrls[0]}
-          />
-        </div>
-        <div className="grid grid-rows-[1fr_2fr_1fr]">
-            <div>
-              <div className="flex items-center">
-                  <span className="flex">
-                    {Array.from({length: hotel.starRating }).map(()=>(
-                      <AiFillStar className='fill-yellow-400'/>
-                    ))}
-                  </span>
-                  <span className="text-sm ml-1">
-                    {hotel.type}
-                  </span>
-              </div>
-              <h2 className="text-2xl font-bold cursor-pointer">{hotel.name}</h2>
-            </div>
-            <div>
-              <div className="line-clamp-4">{hotel.description}</div>
-            </div>
-            <div className="grid grid-cols-2 items-end whitespace-nowrap">
-              <div className="flex gap-1 items-center">
-                    {hotel.facilities.splice(0,3).map((facility, index)=> (
-                      <span className="bg-slate-300 p-2 rounded-lg font-bold text-xs whitespace-nowrap">
-                          {facility}
-                      </span>
-                    ))}
-                    <span className="text-sm">
-                      {hotel.facilities.length > 3 && 
-                        `+${hotel.facilities.length - 3} more`
-                      }
-                    </span>
-              </div>
-            </div>
-        </div>
+    <div className="grid grid-cols-1 xl:grid-cols-[2fr_3fr] border border-slate-300 rounded-lg p-8 gap-8">
+    <div className="w-full h-[300px]">
+      <img
+        src={hotel.imageUrls[0]}
+        className="w-full h-full object-cover object-center"
+      />
     </div>
+    <div className="grid grid-rows-[1fr_2fr_1fr]">
+      <div>
+        <div className="flex items-center">
+          <span className="flex">
+            {Array.from({ length: hotel.starRating }).map(() => (
+              <AiFillStar className="fill-yellow-400" />
+            ))}
+          </span>
+          <span className="ml-1 text-sm">{hotel.type}</span>
+        </div>
+        <Link
+          to={`/detail/${hotel._id}`}
+          className="text-2xl font-bold cursor-pointer"
+        >
+          {hotel.name}
+        </Link>
+      </div>
+
+      <div>
+        <div className="line-clamp-4">{hotel.description}</div>
+      </div>
+
+      <div className="grid grid-cols-2 items-end whitespace-nowrap">
+        <div className="flex gap-1 items-center">
+          {hotel.facilities.slice(0, 3).map((facility) => (
+            <span className="bg-slate-300 p-2 rounded-lg font-bold text-xs whitespace-nowrap">
+              {facility}
+            </span>
+          ))}
+          <span className="text-sm">
+            {hotel.facilities.length > 3 &&
+              `+${hotel.facilities.length - 3} more`}
+          </span>
+        </div>
+        <div className="flex flex-col items-end gap-1">
+          <span className="font-bold">£{hotel.pricePerNight} per night</span>
+          <Link
+            to={`/detail/${hotel._id}`}
+            className="bg-blue-600 text-white h-full p-2 font-bold text-xl max-w-fit hover:bg-blue-500"
+          >
+            View More
+          </Link>
+        </div>
+      </div>
+    </div>
+  </div>
   )
 }
-
-
-// {Array.from({length: hotel.starRating }).map(()=>(
-//   <AiFillStar className='fill-yellow-400'/>
-// ))}
-
 
 export default SearchResultCard
