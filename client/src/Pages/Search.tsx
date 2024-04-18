b0ee9fc1c62dd6880711b73a773733bdeb8522e3
delaquash <olaide1191@gmail.com>
2024-04-18 07:03:51 +0000

    feat: add searchresultcard component

--- client/src/Pages/Search.tsx
+++ client/src/Pages/Search.tsx
@@ -21,7 +21,25 @@ const Search = () => {
         apiClient.searchHotels(searchParams)    
     )
   return (
-    <div>Search</div>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+        <div className="rounded-lg border border-slate-300 p-5 h-fit sticky top-10">
+            <div className='space-y-5'>
+                <h3 className="text-lg font-semibold pb-5 border-b border-slate-500">
+                    Filter By:
+                </h3>
+                {/* Filter conditions */}
+            </div>
+        </div>
+        <div className="flex flex-col gap-5">
+            <div className="flex justify-between items-center">
+                <span className="text-xl font-bold">
+                    {hotelData?.pagination.total} Hotels found
+                    {search.destination ? `in ${search.destination}` : ""}
+                </span>
+            </div>
+            {hotelData?.data.map(()=>())}
+        </div>
+    </div>
   )
 }
 
---
+++ client/src/components/SearchResultCard.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SearchResultCard = () => {
+  return (
+    <div>SearchResultCard</div>
+  )
+}
+
+export default SearchResultCard
