2f059235e0effda2eb90542ec7c6399106a24459
delaquash <olaide1191@gmail.com>
2024-04-18 13:27:45 +0000

    "Add hotel image, star rating, facilities to SearchResultCard; update layout"
    This commit adds a hotel image, star rating display using react-icons, and facilities to the SearchResultCard component. The layout is also updated to use

--- client/src/components/SearchResultCard.tsx
+++ client/src/components/SearchResultCard.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { HotelType } from '../../types/dataTypes';
+import { AiFillStar } from "react-icons/ai"
 
 type SearchResultProps = {
     hotel: HotelType
@@ -7,8 +8,53 @@ type SearchResultProps = {
 
 const SearchResultCard = ({hotel}:SearchResultProps) => {
   return (
-    <div>SearchResultCard</div>
+    <div className="grid grid-cols-1 xl:grid-cols-[2fr_3fr] border border-slate-300 rounded-lg">
+        <div className="w-full h-[300px]">
+          <img 
+              className='object-cover object-center w-full h-full'
+              src={hotel.imageUrls[0]}
+          />
+        </div>
+        <div className="grid grid-rows-[1fr_2fr_1fr]">
+            <div>
+              <div className="flex items-center">
+                  <span className="flex">
+                    {Array.from({length: hotel.starRating }).map(()=>(
+                      <AiFillStar className='fill-yellow-400'/>
+                    ))}
+                  </span>
+                  <span className="text-sm ml-1">
+                    {hotel.type}
+                  </span>
+              </div>
+              <h2 className="text-2xl font-bold cursor-pointer">{hotel.name}</h2>
+            </div>
+            <div>
+              <div className="line-clamp-4">{hotel.description}</div>
+            </div>
+            <div className="grid grid-cols-2 items-end whitespace-nowrap">
+              <div className="flex gap-1 items-center">
+                    {hotel.facilities.splice(0,3).map((facility, index)=> (
+                      <span className="bg-slate-300 p-2 rounded-lg font-bold text-xs whitespace-nowrap">
+                          {facility}
+                      </span>
+                    ))}
+                    <span className="text-sm">
+                      {hotel.facilities.length > 3 && 
+                        `+${hotel.facilities.length - 3} more`
+                      }
+                    </span>
+              </div>
+            </div>
+        </div>
+    </div>
   )
 }
+
+
+// {Array.from({length: hotel.starRating }).map(()=>(
+//   <AiFillStar className='fill-yellow-400'/>
+// ))}
+
 
 export default SearchResultCard
