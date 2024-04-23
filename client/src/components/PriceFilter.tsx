5a676d14cfcdc5da9d18438725b0cacaec2d8746
delaquash <olaide1191@gmail.com>
2024-04-23 09:30:49 +0000

    feat: add max price function

--- client/src/components/PriceFilter.tsx
+++ client/src/components/PriceFilter.tsx
@@ -5,7 +5,18 @@ type FilterProps = {
 
 const PriceFilter = ({onChange, selectedPrice}: FilterProps) => {
   return (
-    <div>PriceFilter</div>
+    <div>
+        <h4 className="font-semibold text-sm mb-2">Price Filter</h4>
+        <select
+            value={selectedPrice}
+            onChange={(event)=>onChange(event.target.value ? parseInt(event.target.value ): undefined)}
+        >
+            <option>Select Max Price</option>
+            {[50, 100, 150, 200, 250, 300, 350, 400, 450, 500].map((price)=> (
+                <option value={price} key={price}>{price}</option>
+            ))}
+        </select>
+    </div>
   )
 }
 
