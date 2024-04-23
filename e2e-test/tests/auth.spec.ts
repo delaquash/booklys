5f3ee7bf75d302c59f5502b9652549b7c5b4e36a
delaquash <olaide1191@gmail.com>
2024-04-23 12:20:57 +0000

    "Update auth.spec.ts with beforeEach hook and remove manage.hotel.spec.ts"
    This commit updates the 'auth.spec.ts' file to use the 'beforeEach' hook instead of 'test' for

--- e2e-test/tests/auth.spec.ts
+++ e2e-test/tests/auth.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 const UI_URL = "http://localhost:5173/"
 
-test('should allow the user to sign in', async ({ page }) => {
+test.beforeEach('should allow the user to sign in', async ({ page }) => {
   await page.goto(UI_URL);
 
   // get the sign in button
