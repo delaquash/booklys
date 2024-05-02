78c0ed5a53efea15b6b50c5419edc7d952d419be
delaquash <olaide1191@gmail.com>
2024-05-02 15:29:31 +0000

    "Add BookingDetails component to display booking details summary; calculate number of nights between check-in and check-out dates"
    This commit adds a new component, BookingDetails, which displays a summary of the booking details, including the

--- client/src/Pages/Booking.tsx
+++ client/src/Pages/Booking.tsx
@@ -3,11 +3,21 @@ import * as apiClient from "../Hooks/api-client";
 import BookingForm from "../forms/BookingForm/BookingForm";
 import { useSearchContext } from "../context/SearchContext";
 import { useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import BookingDetails from "../components/BookingDetails";
+
 
 const Booking = () => {
     const search = useSearchContext()
     const { hotelId } = useParams();
 
+    const [numberOfNights, setNumberOfNights] = useState<number>(1);
+    useEffect(()=> {
+        if(search.checkIn && search.checkOut) {
+            const night = Math.abs(search.checkOut.getTime() - search.checkIn.getTime()/1000*60*60*24)
+            setNumberOfNights(Math.ceil(night))
+        }
+    }, [search.checkIn, search.checkOut])
     const { data: hotel } = useQuery("", ()=>apiClient.fetchMyHotelById(hotelId as string), {
         enabled: !!hotelId
     })
@@ -16,9 +26,17 @@ const Booking = () => {
   return (
     <div className="grid md:grid-cols-[1fr_2fr]">
       <div className="bg-green-200">BOOKING DETAILS SUMMARY</div>
+      <BookingDetails 
+        checkIn={search.checkIn}
+        checkOut={search.checkOut}
+        hotel={hotel}
+        numberOfNights={numberOfNights}
+        adult={search.adultCount}
+        childCount={search.childCount}
+      />
       {currentUser && <BookingForm currentUser={currentUser} />}
     </div>
   )
 }
 
-export default Booking
+export default Booking;
