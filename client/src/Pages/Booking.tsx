bda229e3cb91808970d9f29b686d05e208b63a69
delaquash <olaide1191@gmail.com>
2024-05-04 04:19:23 +0000

    Update Booking component to display "Nothing to display" message when hotel data is not available
    - Change the return statement in the Booking component from <></> to <>Nothing to display</>, which will be displayed when the hotel data

--- client/src/Pages/Booking.tsx
+++ client/src/Pages/Booking.tsx
@@ -22,7 +22,9 @@ const Booking = () => {
         enabled: !!hotelId
     })
     const {data: currentUser } = useQuery("fetchCurrentUser", apiClient.fetchCurrentUser)
-
+    if(!hotel){
+        return <></>
+    }
   return (
     <div className="grid md:grid-cols-[1fr_2fr]">
       <div className="bg-green-200">BOOKING DETAILS SUMMARY</div>
