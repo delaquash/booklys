ffaea56733d82a9e5b5ea57a1d99abea0dc6ba6a
delaquash <olaide1191@gmail.com>
2024-05-05 13:26:17 +0000

    Remove express-validator dependency and usage in Booking.ts
    This commit removes the express-validator dependency and its usage in the Booking.ts file. The `body` function imported from express-validator has been removed.

--- api/src/routes/Booking.ts
+++ api/src/routes/Booking.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { bookings } from "../controllers/Booking";
 
 import verifyToken from "../middleware/auth";
-import { body } from "express-validator";
+
 const router = express.Router();
 
 router.get("/", verifyToken, bookings)
