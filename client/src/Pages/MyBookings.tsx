8da3fa2cdf71490be5af5eb1af7e37fdbbcea543
delaquash <olaide1191@gmail.com>
2024-05-05 18:21:22 +0000

    feat: add my bookings functionality

--- client/src/Pages/MyBookings.tsx
+++ client/src/Pages/MyBookings.tsx
@@ -2,15 +2,51 @@ import * as apiClient from "../Hooks/api-client";
 import { useQuery } from "react-query";
 
 const MyBookings = () => {
-    const { data: hotel } = useQuery("fetchMyHotelBooking", apiClient.fetchMyHotelBookings)
-    if(!hotel || hotel.length){
+    const { data: hotels } = useQuery("fetchMyHotelBooking", apiClient.fetchMyHotelBookings)
+    if(!hotels || hotels.length){
         return (
             <span>No booking found...</span>
         )
     }
   return (
-    <div>
-      
+    <div className="space-y-5">
+      <h1 className="text-3xl font-bold ">My Bookings</h1>
+    {hotels.map((hotel)=> (
+          <div className="grid grid-cols-1 lg:grid-cols-[1fr_3fr] border border-slate-300 rounded-lg p-8 gap-5">
+            <div className="lg:w-full lg:h-[250px]">
+                <img 
+                    alt="Hotel Image"
+                    className="w-full h-full object-cover object-center"
+                    src={hotel.imageUrls[0]}
+                />
+            </div>
+            <div className="flex flex-col gap-4 overflow-y-auto max-h-[300px]">
+                <div className="text-2xl font-bold">
+                    {hotel.name}
+                    <div className="font-normal text-xs">
+                        {hotel.city}, {hotel.country}
+                    </div>
+                </div>
+            </div>
+            {hotel.bookings.map((booking)=> (
+                <div>
+                    <div>
+                        <span className="font-bold mr-2">Dates:</span>
+                            <span>
+                                {new Date(booking.checkIn).toDateString()}
+                                {new Date(booking.checkOut).toDateString()}
+                            </span>
+                    </div>
+                    <div>
+                        <span className="font-bold mr-2">Guests:</span>
+                        <span>
+                            {booking.adultCount} adults, {booking.childCount} children
+                        </span>
+                    </div>
+                </div>
+            ))}
+          </div>
+    ))}
     </div>
   )
 }
