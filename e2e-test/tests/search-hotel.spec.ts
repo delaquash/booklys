61834972e15269db6cb134183a1d0f2a954078b9
delaquash <olaide1191@gmail.com>
2024-05-11 02:55:35 +0000

    feat: unit test for api

--- e2e-test/tests/search-hotel.spec.ts
+++ e2e-test/tests/search-hotel.spec.ts
@@ -3,27 +3,21 @@ import path from 'path';
 
 const UI_URL = "http://localhost:5173/"
 
-
 test.beforeEach('should allow the user to sign in', async ({ page }) => {
     await page.goto(UI_URL);
-  
     // get the sign in button
     await page.getByRole("link", { name: "Sign In" }).click();
-  
     // testing the Sign In header is visible in the page
-    await expect(page.getByRole("heading", {name: "Sign In"})).toBeVisible();
-  
+    await expect(page.getByRole("heading", {name: "Sign In"})).toBeVisible();  
     // testing the sign in page that has email, password, signin button
     // auto fill the input fields with this and 
     await page.locator("[name=email]").fill("piis2@test.com")
     await page.locator("[name=password]").fill("Equarshie857")
     // testing the login button
     await page.getByRole("button", {name: "Login"}).click();
-  
     // Testing the response after login
     await expect(page.getByText("Sign in Successful")).toBeVisible();
   });
-
 
 
   test("should allow user to search for hotel", async ({ page })=> {
@@ -51,7 +45,7 @@ test.beforeEach('should allow the user to sign in', async ({ page }) => {
    })
 
    test("should book hotel", async ({ page })=> {
-    await page.goto(UI_URL);
+      await page.goto(UI_URL);
 
       await page.getByPlaceholder("Where are you going?").fill("Olaide");
       await page.getByRole("button", {name: "Search"}).click();
@@ -63,7 +57,7 @@ test.beforeEach('should allow the user to sign in', async ({ page }) => {
    })
 
    test("should book hotels", async ({ page })=> {
-    await page.goto(UI_URL);
+      await page.goto(UI_URL);
 
       await page.getByPlaceholder("Where are you going?").fill("Olaide");
       await page.getByRole("button", {name: "Search"}).click();
